90efec42c0a5dbc523bfc4faf94df1f281e3bd1d
AnhTuanVip <tuandqhe176834@fpt.edu.vn>
2024-01-20 22:55:37 +0700

    javascript 1

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Hello world!");
+var num = 20.64;
+console.log(num)
+console.log(num.toFixed(3));
+console.log(Math.round(num))
+console.log(Number(num.toFixed(3)));
+var str = "2.2"
+console.log(typeof str)
+console.log(typeof Number(str))
+
+console.log((Math.random(Math.floor < 10)))
+var arr = ["Mot", "Hai" , "Ba"]
+    
+console.log(Math.floor(Math.random() * arr.length))
+
+
